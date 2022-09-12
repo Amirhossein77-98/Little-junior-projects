008f4cff461a6af3874b285a55745e3ff88525d3
Amirhossein77-98 <62775616+Amirhossein77-98@users.noreply.github.com>
2022-09-12 12:53:52 +0000

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -7,9 +7,6 @@ import random
 
 
 pass_length = 0
-uppercase: bool = True
-digit: bool = True
-schar: bool = True
 
 YesAnswers = ["yes", "yep", "yeah", "y", "aha", "ye"]
 NoAnswers = ["no", "nope", "nay", "na", "n", "aa", "ne"]
@@ -29,14 +26,15 @@ def main():
     while run_again in YesAnswers:
 
         ran_or_no: str = input("You want me to generate a password by my standards? or yours? ").lower()
+        
         if (ran_or_no in YesAnswers) or (ran_or_no in YoursAnswer):
             password = totally_random()
             print("\nHere is your " + str(len(password)) + " character password: " + password)
             run_again = input("\nDo you want to make another password or not? ").lower()
-        if (ran_or_no in NoAnswers) or (ran_or_no in MineAnswers):
+        elif (ran_or_no in NoAnswers) or (ran_or_no in MineAnswers):
             print("Ok!\n")
             pass_length = 0
-            password = password_with_user_variables()
+            password = user_parameters()
             print("\nHere is your " + str(len(password)) + " character password: " + password)
             run_again = input("\nDo you want to make another password or not? ").lower()
         else:
@@ -45,15 +43,17 @@ def main():
 
 
 def user_parameters():
-    global pass_length, uppercase, digit, schar
-    containing_uppercase = ''
-    containing_digit = ''
-    containing_schar = ''
+    global pass_length
+    uppercase = True
+    digit = True
+    schar = True
+    containing_uppercase = False
+    containing_digit = False
+    containing_schar = False
 
     while pass_length <= 3:
         try:
-            pass_length = int(input("1. How long do you want it to be?(It's recommended to be 6 characters"
-                                    " at least!): "))
+            pass_length = int(input("1. How long do you want it to be?(It's recommended to be 6 characters at least!): "))
             if pass_length <= 3:
                 print("Sorry but less than 4 characters is not allowed!\n")
             else:
@@ -61,39 +61,44 @@ def user_parameters():
         except ValueError or TypeError:
             print("Your input should be a number!!!\nTry again:\n")
 
-    while containing_uppercase == '':
-        containing_uppercase = input("2. Do you want it to contain uppercase letters or not?(It is recommended to"
-                                     " contain!): ")
+    while not containing_uppercase:
+        containing_uppercase = input("2. Do you want it to contain uppercase letters or not?(It is recommended to contain!): ")
         if containing_uppercase in YesAnswers:
             uppercase = True
+            containing_uppercase = True
         elif containing_uppercase in NoAnswers:
             uppercase = False
+            containing_uppercase = True
         else:
             print("Sorry, but you should answer with 'yes' or 'no'!\n")
-            containing_uppercase = ''
+            containing_uppercase = False
 
-    while containing_digit == '':
+    while not containing_digit:
         containing_digit = input("3. Do you want it to contain numbers or not?(It is recommended to contain!): ")
         if containing_digit in YesAnswers:
             digit = True
+            containing_digit = True
         elif containing_digit in NoAnswers:
             digit = False
+            containing_digit = True
         else:
             print("Sorry, but you should answer with 'yes' or 'no'!\n")
-            containing_digit = ''
+            containing_digit = False
 
-    while containing_schar == '':
-        containing_schar = input("4. Do you want it to contain special characters (like: !@#$) or not?"
-                                 "(It is recommended to contain!): ")
+    while not containing_schar:
+        containing_schar = input("4. Do you want it to contain special characters (like: !@#$) or not? (It is recommended to contain!): ")
         if containing_schar in YesAnswers:
             schar = True
+            containing_schar = True
         elif containing_schar in NoAnswers:
             schar = False
+            containing_schar = True
         else:
             print("Sorry, but you should answer with 'yes' or 'no'!\n")
-            containing_schar = ''
+            containing_schar = False
 
-    password_with_user_variables()
+    password = password_with_user_variables(uppercase, digit, schar)
+    return password
 
 
 def totally_random():
@@ -112,8 +117,8 @@ def totally_random():
     return password
 
 
-def password_with_user_variables():
-    global pass_length, uppercase, digit, schar
+def password_with_user_variables(uppercase, digit, schar):
+    global pass_length
 
     if (uppercase is True) and (digit is True) and (schar is True):
         pass_source = string.ascii_letters + string.digits + string.punctuation
